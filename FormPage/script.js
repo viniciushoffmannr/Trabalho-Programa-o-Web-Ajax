5dd3fdbcfd0c24199e949c31474b8fe18658111e
viniciushoffmannr <95242473+viniciushoffmannr@users.noreply.github.com>
2023-06-22 21:53:23 -0300

    Criando pagina de cadastro do livro

---
+++ FormPage/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $("#botaoCadastrar").click((event) => {
+    event.preventDefault();
+    const data = {
+      userId: 1,
+      id: 1,
+      title: $("#title").val(),
+      body: $("#body").val(),
+    };
+
+    $.ajax({
+      type: "POST",
+      url: "https://jsonplaceholder.typicode.com/posts",
+      data: JSON.stringify(data),
+      contentType: "application/json",
+
+      success: function () {
+        $("#responseArea").text(
+          `O livro "${$("#title").val()}" foi cadastrado com sucesso!`
+        );
+        $("#responseArea").css({ color: "green" });
+        $("#title").val("");
+        $("#body").val("");
+      },
+
+      error: function () {
+        $("#responseArea").text(
+          `Ops! Tivemos um problema do nosso lado ao tentar cadastrar o livro "${$(
+            "#title"
+          ).val()}", tente novamente mais tarde.`
+        );
+        $("#responseArea").css({ color: "red" });
+      },
+    });
+  });
+});
